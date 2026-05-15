f4246b31491897ea2b87a3a9684df9f4a517bde2
MmatySs <103382558+MmatySs@users.noreply.github.com>
2026-05-16 00:00:51 +0200

    Fix indentation and typo in nutrition info section

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,7 +43,7 @@ if ingredients_list:
         st.success('Your Smoothie is ordered,' + ' ' + name_on_order +'!', icon="✅")
     
     # New section to display smoothiefroot nutrition information
-    import requests  
-    smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
-    #st.text(smoothiefroot_response.json())
-    st_df = st.dataframe(data=smoothiefroot_response.json(), use_conteiner_width=true)
+  import requests  
+  smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
+  #st.text(smoothiefroot_response.json())
+  st_df = st.dataframe(data=smoothiefroot_response.json(), use_conteiner_width=true)
